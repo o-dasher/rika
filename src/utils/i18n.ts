75e6af5b1ff06463d4039e260525782dd6b642fd
o-dasher <88356162+o-dasher@users.noreply.github.com>
2022-10-11 14:44:16 -0400

    Fix i18n

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -2,6 +2,18 @@ import { CommandInteraction, User } from "discord.js";
 import L from "../i18n/i18n-node";
 import { TranslationFunctions } from "../i18n/i18n-types";
 
-export const interactionLocalize = (interaction: CommandInteraction) =>
-	((L as Record<string, TranslationFunctions>)[interaction.locale] as TranslationFunctions | undefined)
-	?? L.en
+/**
+ * How many default properties there are on a TranslationFunction.
+ * {length, name}.
+ */
+const default_property_count = 2;
+
+const default_i18n = L.en;
+
+export const interactionI18N = (interaction: CommandInteraction) => {
+	const record = L as Record<string, TranslationFunctions>;
+	const i18n = record[interaction.locale];
+	
+	return Object.getOwnPropertyNames(i18n).length == default_property_count
+	       ? default_i18n : i18n;
+}
